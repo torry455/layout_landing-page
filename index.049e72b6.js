25c724eacbeb921115b23ee7c92b44b35a93d3fd
torry455 <kucerv8@gmail.com>
2025-04-24 12:59:06 +0300

    make build

---
+++ index.049e72b6.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){window.topFunction=function(){document.body.scrollTop=0,document.documentElement.scrollTop=0};var o=document.getElementById("my-btn");window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?o.style.display="block":o.style.display="none"}});
+//# sourceMappingURL=index.049e72b6.js.map
